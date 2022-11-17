9dd54059357b404ee5a36a1dcada47d6ce9a9140
kanaanayt <kanaanabukhadra@gmail.com>
2022-11-17 22:23:36 +0300

    my commit

--- Expense/Expense/Views/MainPage.xaml.cs
+++ Expense/Expense/Views/MainPage.xaml.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Expense.Models;
+using System.IO;
+using System.linq;
 
 namespace Expense.Views
 {
@@ -16,5 +19,33 @@ namespace Expense.Views
         {
             InitializeComponent();
         }
+
+        protected override void OnAppearing()
+        {
+            var expenses = new List<Expense>();
+            var files = Directory.EnumerateFiles(Environment.GetFolderPath(
+                        Environment.SpecialFolder.LocalApplicationData), "*.expenses.txt");
+            foreach (var file in files)
+            {
+                var expense = new Expense
+                {
+                    DatePurchased = File.GetCreationTime(file),
+                    FileName = file,
+                    Name = File.ReadLines(file).First(),
+                    Amount = File.ReadLines(file).Second(),
+                    if (Path.GetExtension(file) = ".groceries.expenses.txt") ExpenseCategory = ExpenseCategoy.Grocery,
+                    if (Path.GetExtension(file) = ".misc.expenses.txt") ExpenseCategory = ExpenseCategoy.Misc,
+                    if (Path.GetExtension(file) = ".shopping.expenses.txt") ExpenseCategory = ExpenseCategoy.Shopping,
+                    if (Path.GetExtension(file) = ".travel.expenses.txt") ExpenseCategory = ExpenseCategoy.Travel,
+                }
+        }
+    }
+
+    private async void ExpenseListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        await Navigation.PushModalAsync(new Expense
+        {
+            BindingContext = (Expense)e.SelectedItem
+        });
     }
 }
