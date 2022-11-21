65db49ad65e00b0b100b39d7de35d3e88e8ce8a0
kanaanayt <kanaanabukhadra@gmail.com>
2022-11-21 19:31:05 +0300

    fixed mainpage stuff

--- Expense/Expense/AppShell.xaml.cs
+++ Expense/Expense/AppShell.xaml.cs
@@ -11,14 +11,14 @@ namespace Expense
         public AppShell()
         {
             InitializeComponent();
-            //MainPageContent = Home;
+            MainPageContent = Home;
             //Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             //Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
-            //await Shell.Current.GoToAsync("//LoginPage");
+            await Shell.Current.GoToAsync("MainPage");
         }
     }
 }
